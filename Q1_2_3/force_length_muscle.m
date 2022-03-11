a6262c133bde461248e873359af1566beb4fdccf
Rachel Leung <rachelleungishappy@gmail.com>
2022-03-11 14:23:31 -0500

    Fixed graph and regression error

--- Q1_2_3/force_length_muscle.m
+++ Q1_2_3/force_length_muscle.m
@@ -1,11 +1,29 @@
-function [force_length_scale_factor] = force_length_muscle(lm)
-
-% Input Parameters
-% lm: muscle (contracile element) length
-
-% Output
-% force-length scale factor
-
-global force_length_regression 
-force_length_scale_factor = feval(force_length_regression, lm);
-end
+function [normalize_PE_force] = force_length_parallel(lm)
+    %%%% TASK 1
+    
+    % Input Parameters
+    % lm: normalized length of muscle (contractile element)
+    
+    % Output
+    % normalized force produced by parallel elastic element
+    
+    % WRITE YOUR CODE HERE
+    lpes = 1;
+    normalize_PE_force = zeros(size(lm)); % This preallocates the space for speed
+    
+    for i = 1:size(lm,2)
+        if i < 100
+            normalize_PE_force(i) = 0;
+        else
+            normalize_PE_force(i) = (3*((lm(i)-lpes)^2))/(0.6+lm(i)-lpes);
+        end
+    end
+    
+    % To Do -------------------------
+    % See if you can get the piecewise to work in the same manner
+    % And also to make it so that it is not hard coded to 100 (100 is basically
+    %%% the value in the array where normalized tendon length is 1 (see
+    %%% plot_curves file)
+    
+    % Your existing code
+    %normalize_PE_force = piecewise(lm < lpes, 0, lm >= lpes, (3*((lm-lpes).^2))./(0.6+lm-lpes));
